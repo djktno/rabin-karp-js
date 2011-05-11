ea7bf25ae898362b728bc4460a1094d5fc8ad82d
djktno <dan@gameattain.com>
2011-05-11 09:57:55 -0600

    Improved calculation of rolling hash.

--- rk.js
+++ rk.js
@@ -5,47 +5,53 @@ var matcher = function(text, pattern, cb) {
   var text_length = text.length;
   var pattern_length = pattern.length;
 
-  var mod = function(a, b) {
-    return a - b * (Math.floor((a/b)));
-  };
+  //If the pattern is longer than the
+  //text, return - there's no way to
+  //compute that.
+  if (pattern_length > text_length)
+    return;
 
-  var h = (Math.pow(radix,(pattern_length - 1))) % prime;
+  //Calculates a mod b even if a < 0
+  var mod = function(a, b) {
+    return (a % b + b) % b;
+  }
+
+  //Precalculate radix^(pattern_length - 1) (mod prime)
+  //This is used to compute the t(s+1) from t(s) "Rolling hash".
+  var h = mod(Math.pow(radix, pattern_length - 1), prime);
   var p = 0;
   var t = 0;
 
-  for (var i = 1; i < pattern_length; i++) {
-    p = ((radix * p) + pattern[i]) % prime;
-    t = ((radix * t) + text[i]) % prime;
+  for (var i = 0; i < pattern_length; i++) {
+    p = mod(radix * p + pattern[i], prime);
+    t = mod(radix * t + text[i], prime);
   }
 
-  for (var s = 0; s <= (text_length - pattern_length); s++) {
-    console.log("Iteration " + s + "=> Text: " + text.slice(s, s+pattern_length).join(''));
-    console.log("P: " + p);
-    console.log("T: " + t);
-    console.log("s: " + s);
-    if (p == t) {
-      console.log("Spurious hit?");
-      console.log("Text: " + text.slice(s, s+pattern_length).join(''));
-      if (pattern.join('') == text.slice(s, s+pattern_length).join('')) {
-        console.log("Verfied match.");
-        match_string = text.slice(s, s+pattern_length).join('');
-        cb(match_string);
-      }
+  if (p == t) {
+    if (pattern.join('') + ":" + text.slice(s, s+pattern_length).join('')) {
+        cb();
     }
-    if (s < (text_length - pattern_length)) {
-      t = ((radix * (t + (text[s+1]*h))) + text[s+pattern_length]) % prime;
-      console.log("Calculating next iteration, ts");
-      console.log("Hash for " + text.slice(s+1, s+pattern_length).join('') + ": " + t);
-      console.log("--------------------------------------------");
+  }
+
+  for (var s = pattern_length; s < text_length; s++) {
+    var p2 = mod(text[s-pattern_length] * h, prime);
+    t = mod(t - p2, prime);
+    t = mod(t * radix, prime);
+    t = mod(t + text[s-1], prime);
+
+    if (p == t){
+      if (pattern.join('') + ":" + text.slice(s, s+pattern_length).join('')) {
+        cb();
+      }
     }
   }
 };
 
-var hit_callback = function(string_matched) {
-  console.log("In callback, matched " + string_matched);
+var hit_callback = function() {
+  console.log("In callback, matched.");
 };
 
 var text = [3,1,4,1,5,9,2,6,5,3,2,6,9,7,9,3];
-var pattern = [2,6,5,3,2];
+var pattern = [2,6];
 
 matcher(text, pattern, hit_callback);
